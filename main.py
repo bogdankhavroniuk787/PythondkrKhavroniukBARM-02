40def7b10acdc7489faa5618c933558905108950
bogdankhavroniuk787 <72021030+bogdankhavroniuk787@users.noreply.github.com>
2020-11-27 19:26:12 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+
+def print(a,n):
+  for b in range(n):
+    if b % 3 :
+       b = "Fizz"
+def prinnt1(a,n):
+    for b in range(n):
+
+        if b % 5 == 0 :
+            b = "Fizz"
+def prinnt2(a,n):
+    for b in range(n):
+
+        if b % 5 == 0 and b % 3 == 0 :
+            b = "Fizz Buzz"
